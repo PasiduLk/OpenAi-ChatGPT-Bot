751646c107b1dbdc2ee5336b5b45be0e853cea85
PasiduLk <130719610+PasiduLk@users.noreply.github.com>
2023-07-03 12:59:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers
+global.OWNER_NAME = 'Pasindu Sehan' // Owner Name
+global.OWNER_NUMBER = ['94703768140'] //Owner Number
+global.BOT_ADMINS = ['94765718443','94703768140'] //Bot admins numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 
 
